98f8dabea9e3d711afb6350ed6b88e5550e44501
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-11-26 02:29:51 +0100

    feat: gamestart command added

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -2,16 +2,17 @@ module.exports = {
     name: 'help',
     description: 'Commande help',
     callback: (message) => {
-        if (!message.member.roles.cache.some(r => r.name === "Staff"))
-            return 'Vous n\'avez pas la permission d\'utiliser cette commande !';
+        if (!message.member.roles.cache.some(r => r.name === "Staff")) {
+            message.message.reply('bonsoir non');
+            return;
+        }
+
         return `
 Voici les commandes supportées:
 
 **!help** - Commande help
 
-
 **__Points:__**
-
 **!add <@participant> <montant>** - Ajoute le <montant> de points au <participant>.
 **!remove <@participant> <montant>** - Retire le <montant> au <participant>.
 **!reset** - Reset les points de tous les participants.
@@ -20,16 +21,12 @@ Voici les commandes supportées:
 **!publicscoreboard <message>** - Affiche le <scoreboard> avec <message> dans le channel.
 **!final** - Fait plein de trucs compliqués faites pas attention svp mrc.
 
-
 **__Démarrage:__**
-
 **!gamestart** - Démarre l'event, donne le rôle "Participant" à ceux qui réagissent.
 **!gameclose** - Ferme les inscriptions à l'event.
 **!gamestop** - Termine l'event, le rôle "Participant" est supprimé.
 
-
 **__Utilitaire:__**
-
 **!announce "Titre" "Description"** - Affiche une annonce avec "Titre" et "Description", ping les "Participants"
         `
     }
