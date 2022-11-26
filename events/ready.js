9403684c06d139a71fe54d1e8348722d4da78dd4
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-11-26 01:03:21 +0100

    Updating the bot from scratch

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-const mongoUtil = require('../mongoUtil.js');
-
-module.exports = async (client) => {
-  await mongoUtil.connectDB(client);
-
-  client.ready = true;
-  client.user.setActivity(`la course`, {type: 'WATCHING'});
-  console.log('Bot prêt');
-};
