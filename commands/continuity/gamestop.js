68bbd5231b4eca0c964872666361e9abfc79f939
Stephane Gelibert <stephane.gelibert@epita.fr>
2022-11-27 00:59:42 +0100

    feat: gamestop command

---
+++ commands/continuity/gamestop.js
@@ -0,0 +1,21 @@
+const participantSchema = require('../../models/participant-schema');
+const roleSchema = require('../../models/role-schema');
+
+module.exports = {
+    name: 'gamestop',
+    description: 'gamestop',
+    callback: async (message) => {
+        if (!message.member.roles.cache.some(r => r.name === "Staff")) {
+            message.message.reply('bonsoir non');
+            return;
+        }
+        const msg = message.message.content.slice(message.message.content.indexOf(' ') + 1);
+        const role = message.guild.roles.cache.find(r => r.name === 'Participant');
+        await roleSchema.deleteOne({ name: 'Participant' });
+        await participantSchema.deleteMany();
+        await role.delete();
+        message.message.delete();
+        await message.channel.send(msg);
+    },
+};
+  
